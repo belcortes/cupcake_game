838b6fd7b4c936154447dc4a78c4fc032a6487e9
Isabel Cortes <belcortes@gmail.com>
2013-07-14 18:46:35 -0400

    redirect user to cupcakes page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def stored_location_for(resource)
+    nil
+  end
+
+  def after_sign_in_path_for(resource)
+    cupcakes_path
+  end
 end
